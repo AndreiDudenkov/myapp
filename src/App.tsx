cca596994e3d65f4872c653f978d6cc43a06b423
AndreiDudenkov <andrei634d@gmail.com>
2023-04-12 00:45:43 +0300

    buttons"ON" "OFF" + useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {Indicator} from './components/Indicator/Indicator';
 
 function App () {
     console.log('App rendering')
     return (
         <div id={"app"}>
-            <PageTitle title={'This is App component'} />
-            <PageTitle title={'My friends'} />
-            <Accordion titleValue={'Title menu'} deployed={false} />
-            <Accordion titleValue={'Title users'} deployed={false}/>
-            <Rating value={1} />
-            <Rating value={2} />
-            <Rating value={3} />
-            <Rating value={4} />
-            <Rating value={5} />
+            {/*<PageTitle title={'This is App component'} />*/}
+            {/*<PageTitle title={'My friends'} />*/}
+            {/*<Accordion titleValue={'Title menu'} deployed={false} />*/}
+            {/*<Accordion titleValue={'Title users'} deployed={false}/>*/}
+            {/*<Rating value={1} />*/}
+            {/*<Rating value={2} />*/}
+            {/*<Rating value={3} />*/}
+            {/*<Rating value={4} />*/}
+            {/*<Rating value={5} />*/}
+            <Indicator value={false} /> <br/>
+            <Indicator value={false} /><br/>
+            <Indicator value={false} /><br/>
+            <Indicator value={false} /><br/>
+            <Indicator value={false} /><br/>
+            <Indicator value={false} /><br/>
+            <Indicator value={false} />
         </div>
     );
 }
---
+++ src/components/Indicator/Indicator.tsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {log} from 'util';
+
+type IndicatorType = {
+    value: boolean
+}
+
+export const Indicator = (props:IndicatorType) => {
+    let [status, setStatus] = useState(false)
+
+
+    const onStyle = {
+        marginLeft: "10px",
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        padding: "2px",
+        display: "inline-block",
+        backgroundColor: status ? "green" : "white"
+
+    }
+    const offStyle = {
+        marginLeft: "10px",
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        padding: "2px",
+        display: "inline-block",
+        backgroundColor: status ? "white" : "red"
+
+
+
+    }
+    const indicatorStyle = {
+        marginLeft: "10px",
+        width: "10px",
+        height: "10px",
+        border: "1px solid black",
+        borderRadius: "5px",
+        display: "inline-block",
+        backgroundColor: status ? "green" : "red"
+    }
+
+    return (
+        <>
+            <div style={onStyle} onClick={()=>setStatus(true)}>ON</div>
+            <div style={offStyle} onClick={()=>setStatus(false)}>OFF</div>
+            <div style={indicatorStyle}> </div>
+        </>
+    )
+}
