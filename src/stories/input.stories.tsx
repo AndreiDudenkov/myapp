ef4363817c0487e924dffee0efdd120de3fceee7
AndreiDudenkov <andrei634d@gmail.com>
2023-05-08 17:20:42 +0300

    storybook CONTROLLED components

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
 import {Simulate} from 'react-dom/test-utils';
+import {action} from '@storybook/addon-actions'
 import input = Simulate.input;
 
 export  default {
@@ -26,6 +27,37 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 }
 
 
+export const ControlledInput =() => {
+    const [parentVale, setParentValue] = useState('')
+    const onchangeHandler =(e:ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
 
+    return <> <input value={parentVale} onChange={onchangeHandler}/> </>
+}
+
+export const ControlledCheckBox =() => {
+    const [parentVale, setParentValue] = useState(true)
+
+    const onchangeHandler =(e:ChangeEvent<HTMLInputElement> ) => {
+        setParentValue(e.currentTarget.checked)
+    }
+
+    return <> <input type={'checkbox'} checked={parentVale} onChange={onchangeHandler}/> </>
+}
+
+export const ControlledSelect =() => {
+    const [parentVale, setParentValue] = useState<string | undefined>('2')
+    const onchangeHandler =(e:ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <>
+        <select value={parentVale} onChange={onchangeHandler}>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Moskow</option>
+        <option value={'3'}>Kiev</option>
+    </select> </>
+}
 
 export const ControlledInputWithFixedValue = () =>  <input value={'Andrei'}/>
